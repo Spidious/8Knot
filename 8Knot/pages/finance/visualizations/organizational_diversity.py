6cc5c5d01e2bcebccbec6325e1a0d7a7e8c0b016
xAerrok <123225346+xAerrok@users.noreply.github.com>
2023-12-16 23:27:04 -0600

    Add files via upload

--- 8Knot/pages/finance/visualizations/organizational_diversity.py
+++ 8Knot/pages/finance/visualizations/organizational_diversity.py
@@ -8,49 +8,15 @@ import logging
 from dateutil.relativedelta import *  # type: ignore
 import plotly.express as px
 from pages.utils.graph_utils import get_graph_time_values, color_seq
-from queries.contributors_query import contributors_query as ctq
+from queries.company_query import company_query as cpq
 import io
 from cache_manager.cache_manager import CacheManager as cm
 from pages.utils.job_utils import nodata_graph
 import time
+import datetime as dt
 
-"""
-NOTE: VARIABLES TO CHANGE:
-
-(1) PAGE
-(2) VIZ_ID
-(3) gc_VISUALIZATION
-(4) TITLE OF VISUALIZATION
-(5) CONTEXT OF GRAPH
-(6) IDs of Dash components
-(6) NAME_OF_VISUALIZATION_graph
-(7) COLUMN_WITH_DATETIME
-(8) COLUMN_WITH_DATETIME
-(9) COLUMN_TO_SORT_BY
-(10) Comments before callbacks
-(11) QUERY_USED, QUERY_NAME, QUERY_INITIALS
-
-NOTE: IMPORTING A VISUALIZATION INTO A PAGE
-(1) Include the visualization file in the visualization folder for the respective page
-(2) Import the visualization into the page_name.py file using "from .visualizations.visualization_file_name import gc_visualization_name"
-(3) Add the card into a column in a row on the page
-
-NOTE: ADDITIONAL DASH COMPONENTS FOR USER GRAPH CUSTOMIZATIONS
-
-If you add Dash components (ie dbc.Input, dbc.RadioItems, dcc.DatePickerRange...) the ids, html_for, and targets should be in the
-following format: f"component-identifier-{PAGE}-{VIZ_ID}"
-
-NOTE: If you change or add a new query, you need to do "docker system prune -af" before building again
-
-For more information, check out the new_vis_guidance.md
-"""
-
-
-# TODO: Remove unused imports and edit strings and variables in all CAPS
-# TODO: Remove comments specific for the template
-
-PAGE = "finance"  # EDIT FOR CURRENT PAGE
-VIZ_ID = "Organizational_diversity"  # UNIQUE IDENTIFIER FOR VIZUALIZATION
+PAGE = "funding"  # EDIT FOR CURRENT PAGE
+VIZ_ID = "org-diversity"  # UNIQUE IDENTIFIER FOR VIZUALIZATION
 
 gc_Organizational_diversity = dbc.Card(
     [
@@ -64,7 +30,12 @@ gc_Organizational_diversity = dbc.Card(
                 dbc.Popover(
                     [
                         dbc.PopoverHeader("Graph Info:"),
-                        dbc.PopoverBody("Context of graph"),
+                        dbc.PopoverBody(
+                            """
+                            Visualizes the amount of contributions made to the project by\n
+                            organizations relative to each other.
+                            """
+                        ),
                     ],
                     id=f"popover-{PAGE}-{VIZ_ID}",
                     target=f"popover-target-{PAGE}-{VIZ_ID}",
@@ -79,27 +50,37 @@ gc_Organizational_diversity = dbc.Card(
                         dbc.Row(
                             [
                                 dbc.Label(
-                                    "Date Interval:",
-                                    html_for=f"date-radio-{PAGE}-{VIZ_ID}",
-                                    width="auto",
+                                    "Contributors Required:",
+                                    html_for=f"contributions-required-{PAGE}-{VIZ_ID}",
+                                    width={"size": "auto"},
                                 ),
                                 dbc.Col(
-                                    [
-                                        dbc.RadioItems(
-                                            id=f"date-radio-{PAGE}-{VIZ_ID}",
-                                            options=[
-                                                {
-                                                    "label": "Trend",
-                                                    "value": "D",
-                                                },  # TREND IF LINE, DAY IF NOT
-                                                # {"label": "Week","value": "W",}, UNCOMMENT IF APPLICABLE
-                                                {"label": "Month", "value": "M"},
-                                                {"label": "Year", "value": "Y"},
-                                            ],
-                                            value="M",
-                                            inline=True,
-                                        ),
-                                    ]
+                                    dbc.Input(
+                                        id=f"contributions-required-{PAGE}-{VIZ_ID}",
+                                        type="number",
+                                        min=1,
+                                        max=50,
+                                        step=1,
+                                        value=3,
+                                        size="sm",
+                                    ),
+                                    className="me-2",
+                                    width=2,
+                                ),
+                            ],
+                            align="center",
+                        ),
+                        dbc.Row(
+                            [
+                                dbc.Col(
+                                    dcc.DatePickerRange(
+                                        id=f"date-picker-range-{PAGE}-{VIZ_ID}",
+                                        min_date_allowed=dt.date(2005, 1, 1),
+                                        max_date_allowed=dt.date.today(),
+                                        initial_visible_month=dt.date(dt.date.today().year, 1, 1),
+                                        clearable=True,
+                                    ),
+                                    width="auto",
                                 ),
                                 dbc.Col(
                                     dbc.Button(
@@ -113,6 +94,7 @@ gc_Organizational_diversity = dbc.Card(
                                 ),
                             ],
                             align="center",
+                            justify="between",
                         ),
                     ]
                 ),
@@ -134,25 +116,25 @@ def toggle_popover(n, is_open):
     return is_open
 
 
-# callback for VIZ TITLE graph
+# callback for Organization Diversity graph
 @callback(
     Output(f"{PAGE}-{VIZ_ID}", "figure"),
-    # Output(f"check-alert-{PAGE}-{VIZ_ID}", "is_open"), USE WITH ADDITIONAL PARAMETERS
     # if additional output is added, change returns accordingly
     [
         Input("repo-choices", "data"),
-        Input(f"date-radio-{PAGE}-{VIZ_ID}", "value"),
-        # add additional inputs here
+        Input(f"contributions-required-{PAGE}-{VIZ_ID}", "value"),
+        Input(f"date-picker-range-{PAGE}-{VIZ_ID}", "start_date"),
+        Input(f"date-picker-range-{PAGE}-{VIZ_ID}", "end_date"),
     ],
     background=True,
 )
-def time_to_first_response_graph(repolist, interval):
+def organizational_diversity_ratio_graph(repolist, num, start_date, end_date):
     # wait for data to asynchronously download and become available.
     cache = cm()
-    df = cache.grabm(func=ctq, repos=repolist)
+    df = cache.grabm(func=cpq, repos=repolist)
     while df is None:
         time.sleep(1.0)
-        df = cache.grabm(func=ctq, repos=repolist)
+        df = cache.grabm(func=cpq, repos=repolist)
 
     start = time.perf_counter()
     logging.warning(f"{VIZ_ID}- START")
@@ -163,38 +145,59 @@ def time_to_first_response_graph(repolist, interval):
         return nodata_graph
 
     # function for all data pre processing, COULD HAVE ADDITIONAL INPUTS AND OUTPUTS
-    df = process_data(df, interval)
+    df = process_data(df, num, start_date, end_date)
 
-    fig = create_figure(df, interval)
+    fig = create_figure(df)
 
     logging.warning(f"{VIZ_ID} - END - {time.perf_counter() - start}")
     return fig
 
 
-def process_data(df: pd.DataFrame, interval):
-    """Implement your custom data-processing logic in this function.
-    The output of this function is the data you intend to create a visualization with,
-    requiring no further processing."""
+def process_data(df: pd.DataFrame, num, start_date, end_date):
 
     # convert to datetime objects rather than strings
-    # ADD ANY OTHER COLUMNS WITH DATETIME
-    df["created_at"] = pd.to_datetime(df["created_at"], utc=True)
+    df["created"] = pd.to_datetime(df["created"], utc=True)
 
     # order values chronologically by COLUMN_TO_SORT_BY date
-    df = df.sort_values(by="created_at", axis=0, ascending=True)
+    df = df.sort_values(by="created", axis=0, ascending=True)
 
-    """LOOK AT OTHER VISUALIZATIONS TO SEE IF ANY HAVE A SIMILAR DATA PROCESS"""
+    # filter values based on date picker
+    if start_date is not None:
+        df = df[df.created >= start_date]
+    if end_date is not None:
+        df = df[df.created <= end_date]
+    
+    # creates list of unique companies and flattens list result
+    companies = df.cntrb_company.str.split(" , ").explode("cntrb_company").unique().tolist()
 
+    # creates df of organizations and counts
+    df = pd.DataFrame(companies, columns=["organizations"]).value_counts().to_frame().reset_index()
+    
+    df = df.rename(columns={0: "occurences"})
+    
+    # changes the name of the company if under a certain threshold
+    df.loc[df.occurences < num, "organizations"] = "Other"
+    
+    # groups others together for final counts
+    df = (
+        df.groupby(by="organizations")["occurences"]
+        .sum()
+        .reset_index()
+        .sort_values(by=["occurences"], ascending=False)
+        .reset_index(drop=True)
+    )
+    
     return df
 
 
-def create_figure(df: pd.DataFrame, interval):
-    # time values for graph
-    x_r, x_name, hover, period = get_graph_time_values(interval)
-
+def create_figure(df: pd.DataFrame):
+    
     # graph generation
-    fig = fig
-
-    """LOOK AT OTHER VISUALIZATIONS TO SEE IF ANY HAVE A SIMILAR GRAPH"""
+    fig = px.pie(df, values="occurences", names="organizations")
+    fig.update_traces(
+        textposition="inside",
+        textinfo="percent+label",
+        hovertemplate="%{label} <br>Contributions: %{value}"
+    )
 
     return fig
